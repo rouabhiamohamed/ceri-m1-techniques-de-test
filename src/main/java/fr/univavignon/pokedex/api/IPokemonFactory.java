480d1a721e935775345f92432437ea1dac33795f
rouabhiamohamed <mohamed.rouabhia2@alumni.univ-avignon.fr>
2024-12-02 09:54:41 +0100

    checkstyle

--- src/main/java/fr/univavignon/pokedex/api/IPokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokemonFactory.java
@@ -2,14 +2,14 @@ package fr.univavignon.pokedex.api;
 
 /**
  * Factory interface for class that aims to create Pokemon instance.
- * 
+ *
  * @author fv
  */
 public interface IPokemonFactory {
 
 	/**
 	 * Creates a pokemon instance computing it IVs.
-	 * 
+	 *
 	 * @param index Pokemon index.
 	 * @param cp Pokemon CP.
 	 * @param hp Pokemon HP.
@@ -18,5 +18,5 @@ public interface IPokemonFactory {
 	 * @return Created pokemon instance.
 	 */
 	Pokemon createPokemon(int index, int cp, int hp, int dust, int candy);
-	
+
 }
