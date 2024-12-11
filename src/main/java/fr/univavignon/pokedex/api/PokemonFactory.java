fbc3db0810605a0fb8ee4b1bb68d8ae140cceb0a
rouabhiamohamed <mohamed.rouabhia2@alumni.univ-avignon.fr>
2024-12-11 16:08:54 +0100

    tp5

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -5,58 +5,68 @@ import java.util.Map;
 import java.util.Random;
 
 public class PokemonFactory implements IPokemonFactory {
-
-	private static final Map<Integer, String> POKEMON_NAMES = new HashMap<>() {{
-		put(0, "Bulbizarre");
-		put(1, "Herbizarre");
-		put(2, "Florizarre");
-		put(3, "Salamèche");
-		put(4, "Reptincel");
-		put(5, "Dracaufeu");
-		put(6, "Carapuce");
-		put(7, "Carabaffe");
-		put(8, "Tortank");
-		put(133, "Aquali");
-
-	}};
-
-	/**
-	 *
-	 * @return Un tableau de 3 valeurs (attaque, défense, stamina)
-	 */
-	private int[] generateIVs() {
-		Random random = new Random();
-		int attack = random.nextInt(16);     // IV entre 0 et 15
-		int defense = random.nextInt(16);    // IV entre 0 et 15
-		int stamina = random.nextInt(16);    // IV entre 0 et 15
-		return new int[]{attack, defense, stamina};
-	}
-
-	/**
-	 * @param ivs Tableau des IV
-	 * @return Valeur IV calculée
-	 */
-	private double calculateTotalIV(int[] ivs) {
-		return ((ivs[0] + ivs[1] + ivs[2]) / 45.0) * 100;
-	}
-
-	@Override
-	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-		String name = POKEMON_NAMES.getOrDefault(index, "Inconnu");
-
-		int[] ivs = generateIVs();
-		double iv = calculateTotalIV(ivs);
-		return new Pokemon(
-				index,
-				name,
-				ivs[0] * 15,
-				ivs[1] * 15,
-				ivs[2] * 15,
-				cp,
-				hp,
-				dust,
-				candy,
-				iv
-		);
-	}
+  /**
+   * Une map constante associant les indices des Pokémon à leurs noms respectifs.
+   * <p>
+   * Cette map est utilisée pour fournir les noms des Pokémon en fonction de leurs indices.
+   * Elle est initialisée de manière statique avec des valeurs prédéfinies pour des cas
+   * particuliers, comme Pikachu de Sacha (-1) et MISSINGNO (0).
+   * </p>
+    *
+    * <ul>
+   * <li>Clé : Un entier représentant l'indice du Pokémon.</li>
+   * <li>Valeur : Une chaîne de caractères représentant le nom du Pokémon.</li>
+   * </ul>
+   *
+   * <strong>Note :</strong> Cette map est déclarée comme constante avec `final` pour
+   * garantir qu'elle ne sera jamais remplacée après son initialisation.
+   */
+  private static final Map<Integer, String> POKEMON_NAMES = new HashMap<>() {{
+   put(0, "Bulbizarre");
+    put(1, "Herbizarre");
+   put(2, "Florizarre");
+   put(3, "Salamèche");
+   put(4, "Reptincel");
+   put(5, "Dracaufeu");
+   put(6, "Carapuce");
+   put(7, "Carabaffe");
+   put(8, "Tortank");
+   put(133, "Aquali");
+   }};
+  /**
+   *
+   * @return Un tableau de 3 valeurs (attaque, défense, stamina)
+   */
+   private int[] generateIVs() {
+   Random random = new Random();
+   int attack = random.nextInt(16);     // IV entre 0 et 15
+   int defense = random.nextInt(16);    // IV entre 0 et 15
+   int stamina = random.nextInt(16);    // IV entre 0 et 15
+   return new int[]{attack, defense, stamina};
+  }
+   /**
+    * @param ivs Tableau des IV
+   * @return Valeur IV calculée
+    */
+   private double calculateTotalIV(int[] ivs) {
+    return ((ivs[0] + ivs[1] + ivs[2]) / 45.0) * 100;
+   }
+   @Override
+   public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+    String name = POKEMON_NAMES.getOrDefault(index, "Inconnu");
+    int[] ivs = generateIVs();
+    double iv = calculateTotalIV(ivs);
+   return new Pokemon(
+    index,
+    name,
+    ivs[0] * 15,
+    ivs[1] * 15,
+    ivs[2] * 15,
+    cp,
+    hp,
+    dust,
+     candy,
+     iv
+     );
+   }
 }
--- src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
@@ -7,52 +7,64 @@ import java.util.Random;
 import org.apache.commons.collections4.map.UnmodifiableMap;
 
 public class RocketPokemonFactory implements IPokemonFactory {
-	
-	private static Map<Integer, String> index2name;
-	static {
-		Map<Integer, String> aMap = new HashMap<Integer, String>();
+    /**
+    * Une association entre les indices des Pokémon et leurs noms correspondants.
+    * <p>
+    * Cette map permet de récupérer le nom d'un Pokémon à partir de son indice.
+    * Elle inclut des entrées prédéfinies pour des cas spéciaux, comme Pikachu de Sacha (-1)
+    * et MISSINGNO (0). La map est immuable afin de garantir la cohérence dans son utilisation.
+    * </p>
+    *
+    * <ul>
+    * <li>Clé : Un entier représentant l'indice du Pokémon.</li>
+    * <li>Valeur : Une chaîne de caractères représentant le nom du Pokémon.</li>
+   * </ul>
+    */
+
+    private static Map<Integer, String> index2name;
+    static {
+   Map<Integer, String> aMap = new HashMap<Integer, String>();
         aMap.put(-1, "Ash's Pikachu");
         aMap.put(0, "MISSINGNO");
         aMap.put(1, "Bulbasaur");
         //TODO : Gotta map them all !
         index2name = UnmodifiableMap.unmodifiableMap(aMap);
-	}
-	
-	private static int generateRandomStat() {
-		int total = 0;
-		for(int i=0; i < 1000000; i++)
-		{
-			Random rn = new Random();
-		    int r = rn.nextInt(2);
-		    total = total + r;
-		}
-		return total / 10000;
-	}
+   }
 
-	@Override
-	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-		String name;
-		if(!index2name.containsKey(index)) {
-			name = index2name.get(0);
-		} else {
-			name = index2name.get(index);
-		}
-		int attack;
-		int defense;
-		int stamina;
-		double iv;
-		if(index < 0) {
-			attack = 1000;
-			defense = 1000;
-			stamina = 1000;
-			iv = 0;
-		} else {
-			attack = RocketPokemonFactory.generateRandomStat();
-			defense = RocketPokemonFactory.generateRandomStat();
-			stamina = RocketPokemonFactory.generateRandomStat();
-			iv = 1;
-		}
-		return new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
-	}
+   private static int generateRandomStat() {
+    int total = 0;
+       for(int i=0; i < 1000000; i++)
+   {
+    Random rn = new Random();
+      int r = rn.nextInt(2);
+     total = total + r;
+    }
+     return total / 10000;
+    }
 
-}
+    @Override
+   public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+    String name;
+      if(!index2name.containsKey(index)) {
+    name = index2name.get(0);
+    } else {
+     name = index2name.get(index);
+    }
+    int attack;
+    int defense;
+    int stamina;
+    double iv;
+    if(index < 0) {
+     attack = 1000;
+     defense = 1000;
+     stamina = 1000;
+     iv = 0;
+     } else {
+     attack = RocketPokemonFactory.generateRandomStat();
+     defense = RocketPokemonFactory.generateRandomStat();
+     stamina = RocketPokemonFactory.generateRandomStat();
+     iv = 1;
+     }
+    return new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
+   }
+   }
