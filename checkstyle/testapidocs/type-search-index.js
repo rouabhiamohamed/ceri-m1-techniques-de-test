d90d1d7de801be48d80309035e4aeb19651080a2
CircleCI <ci@circleci.com>
2024-12-18 22:08:04 +0000

    Update documentation and reports

---
+++ checkstyle/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"fr.univavignon.pokedex.api","l":"AbstractPokedexTest"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"fr.univavignon.pokedex.api","l":"PokedexTest"},{"p":"fr.univavignon.pokedex.api","l":"PokemonTrainerFactoryTest"}]
